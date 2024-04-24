3e4d4d2fa4272f8ba5b8f6961e933d3c993cc054
Tom Hofmann <tom@scheidweg.net>
2024-04-24 21:13:18 +0200

    add 2024 SBB schedule

--- fahrplan/__init__.py
+++ fahrplan/__init__.py
@@ -2,4 +2,5 @@ from . import gtfs_reader
 import os
 
 fahrplan_2022 = gtfs_reader.read_json_schedule(os.path.join(__path__[0], "./fahrplan_2022.json"))
-latest = fahrplan_2022
+fahrplan_2024 = gtfs_reader.read_json_schedule(os.path.join(__path__[0], "./fahrplan_2024.json"))
+latest = fahrplan_2024
