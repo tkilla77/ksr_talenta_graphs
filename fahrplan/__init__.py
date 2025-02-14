c696fcb1cb39467a00e1d10db95784d651fdacb7
Tom Hofmann <tom@scheidweg.net>
2025-02-14 10:16:29 +0100

    add 2025 schedule

--- fahrplan/__init__.py
+++ fahrplan/__init__.py
@@ -3,4 +3,5 @@ import os
 
 fahrplan_2022 = gtfs_reader.read_json_schedule(os.path.join(__path__[0], "./fahrplan_2022.json"))
 fahrplan_2024 = gtfs_reader.read_json_schedule(os.path.join(__path__[0], "./fahrplan_2024.json"))
-latest = fahrplan_2024
+fahrplan_2025 = gtfs_reader.read_json_schedule(os.path.join(__path__[0], "./fahrplan_2025.json"))
+latest = fahrplan_2025
